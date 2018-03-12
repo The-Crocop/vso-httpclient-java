bdb05baaa30a402104cb2c2ff102f75b1376ee07
asnapper <matthias.loeffel@gmail.com>
2018-03-12 10:40:18 +0100

    import namespace

--- vars/pullRequestStatus.groovy
+++ vars/pullRequestStatus.groovy
@@ -1,3 +1,5 @@
+import com.microsoft.alm.teamfoundation.sourcecontrol.webapi
+
 def call(body) {
 
     def config = [:]
@@ -6,4 +8,15 @@ def call(body) {
     body()
 
     sh "echo 'test ${config.pullRequestId} ${config.pullRequestStatus}'"
+
+    //GitPullRequestStatus status = new GitPullRequestStatus();
+    //GitStatusContext context = new GitStatusContext();
+    //context.setGenre("Foo");
+    //context.setName("Bar");
+    //status.setContext(context);
+    //status.setState(statusState);
+    //status.setDescription(statusDescription);
+    //status.setTargetUrl(targetUrl);
+    //// Don't set iterationId on the status
+    //gitHttpClient.createPullRequestStatus(status, gitRepo.getId(), pullRequest.getPullRequestId());
 }
